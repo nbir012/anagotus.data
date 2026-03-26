07632fcf43e3bfdfd2a3ff2e5b724545b6ac4a54
Neil Birrell <nbir012@aucklanduni.ac.nz>
2026-03-27 00:14:06 +1300

    prep for publication

--- R/df_anagotus.R
+++ R/df_anagotus.R
@@ -2,8 +2,8 @@
 #'
 #' Anagotus is a genus of large, flightless weevils (Coleoptera: Curculionidae) endemic to New Zealand. The dataset contains label data for Anagotus specimens located in museum collections in New Zealand and overseas. The variables are:
 #'
-#' @format ## `anagotus.data`
-#' A tibble with 2,147 rows and 12 variables:
+#' @format ## `df_anagotus`
+#' A tibble with 2,145 rows and 15 variables:
 #' \describe{
 #'   \item{institution_code}{chr Code for the institution housing the specimen. Codes have been standardised as:
 #' NZAC = New Zealand Arthropod Collection, New Zealand;
@@ -18,19 +18,19 @@
 #'   \item{recorded_by}{chr The collector of the specimen, if known.}
 #'   \item{year}{num The year the specimen was collected, if known.}
 #'   \item{month}{num The month the specimen was collected, if known.}
-#'   \item{day}{num The day the specimen was collected, if known.}
-#'   \item{decimal_latitude}{chr The latitude in decimal degrees where the specimen was collected. This is often estimated from a place name.}
-#'   \item{decimal_longitude}{chr The longitude in decimal degrees where the specimen was collected. This is often estimated from a place name.}
+#'   \item{decimal_latitude}{num The latitude in decimal degrees where the specimen was collected. This is often estimated from a place name.}
+#'   \item{decimal_longitude}{num The longitude in decimal degrees where the specimen was collected. This is often estimated from a place name.}
 #'   \item{location}{chr The textual description of the location where the specimen was collected.}
 #'   \item{species}{chr The latin binomial or tag name of the specimen. In some cases, where the specific epithet is indicated by an *, these are manuscript names from Willy Kuschel and should not be used in formal publications without checking with a taxonomic expert.}
 #'   \item{generic_name}{chr The genus name of the specimen.}
 #'   \item{specific_name}{chr The species name of the specimen. In some cases, where the specific epithet is indicated by an *, these are manuscript names from Willy Kuschel and should not be used in formal publications without checking with a taxonomic expert.}
+#'   \item{microhabitat}{chr The habitat or microhabitat associated with the specimen, as per the label data.}
 #'   \item{data_origin}{chr How the data was sourced. The values are:
 #' nwb_<institution_code>_digitised = data digitised by Neil Birrell from physical specimen labels in museum collections;
 #' curator_database_checked = data downloaded from museum collection databases by a museum curators. This is the case for CMNZ which was undergoing renovations at the time and a physical visit was not possible.
 #' tfbis_<institution_code>_digitised = data downloaded from the Terrestrial and Freshwater Biodiversity Information System (TFBIS) managed by Manaaki Whenua - Landcare Research, New Zealand. This includes a variety of collections. In the case of NZAC it may contain duplicates of what has been digitised by Neil Birrell. As there was no unique identifier attached to specimens for the tfbis data, it is not possible to cross check for duplicates. If this is important to you, you should use only the nwb_<institution_code>_digitised values which were comprehensive checks by one researcher.}
 #'   \item{df_origin}{chr The dataframe where this value originated from. This will correspond to a particular csv which can be found in the data-raw folder.}
-#'   \item{microhabitat}{chr The habitat or microhabitat associated with the specimen, as per the label data.}
+#'   \item{day}{int The day the specimen was collected, if known.}
 #' }
 #' @source {Neil Birrell} Collected from museum collections and specimen labels.
 #' @source [Manaaki Whenua - Landcare Research, New Zealand. Terrestrial and Freshwater Biodiversity Information System (TFBIS) Large Weevils.](https://www.landcareresearch.co.nz/tools-and-resources/collections/new-zealand-arthropod-collection-nzac/tfbis-funded-specimen-information/large-weevils)
--- README.md
+++ README.md
@@ -1,3 +1,16 @@
+# anagotus.data
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+Label data for *Anagotus* spp. specimens located in museum collections.
+
+*Anagotus* is a genus of large, flightless weevils (Coleoptera:
+Curculionidae) endemic to New Zealand. This package provides a dataset of
+label data for *Anagotus* specimens housed in museum collections in New
+Zealand and overseas, including temporal and spatial information where
+available.
+
 ```
          ..                                  ..         
         .*...                              ..==.        
@@ -41,3 +54,37 @@
         .+:  -:-.                            =-.        
         ...                                  ..         
 ```
+
+## Installation
+
+You can install anagotus.data from [GitHub](https://github.com/nbir012/anagotus.data):
+
+```r
+# install.packages("pak")
+pak::pak("nbir012/anagotus.data")
+```
+
+## Usage
+
+```r
+library(anagotus.data)
+
+df_anagotus
+```
+
+The dataset contains 2,145 specimen records with 15 variables including
+institution codes, collection dates, coordinates, locality descriptions,
+and species identifications. See `?df_anagotus` for full documentation of
+all variables.
+
+## Data sources
+
+- Specimen label data digitised by Neil Birrell from museum collections in
+  New Zealand and overseas.
+- [Terrestrial and Freshwater Biodiversity Information System
+  (TFBIS)](https://www.landcareresearch.co.nz/tools-and-resources/collections/new-zealand-arthropod-collection-nzac/tfbis-funded-specimen-information/large-weevils)
+  managed by Manaaki Whenua – Landcare Research, New Zealand.
+
+## License
+
+MIT
